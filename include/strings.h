deb3f224d993b5088c7d00148496df974c7e7dfc
Captain-Ford <jason99robinson@gmail.com>
2023-08-26 14:12:59 +0200

    Relearn moves from menu, trainer pokeball anims

--- include/strings.h
+++ include/strings.h
@@ -3036,4 +3036,6 @@ extern const u8 gText_BoxName[];
 extern const u8 gText_PkmnsNickname[];
 extern const u8 gText_TellHimTheWords[];
 
+extern const u8 gText_Moves_Menu[];
+
 #endif // GUARD_STRINGS_H
--- src/strings.c
+++ src/strings.c
@@ -1833,3 +1833,4 @@ const u8 gText_PokeBalls[] = _("POKé BALLS");
 const u8 gText_Berry[] = _("BERRY");
 const u8 gText_Berries[] = _("BERRIES");
 const u8 gText_Nickname[] = _("NICKNAME");
+const u8 gText_Moves_Menu[] = _("RELEARN MOVES");
