40c5921ea4cc29531c414fa08a0d2469f2b95d9a
Captain-Ford <jason99robinson@gmail.com>
2023-08-29 22:35:27 +0200

    Triple layer metatiles fix

--- src/shop.c
+++ src/shop.c
@@ -857,6 +857,7 @@ static void BuyMenuDrawMapMetatile(s16 x, s16 y, const u16 *src, u8 metatileLaye
             BuyMenuDrawMapMetatileLayer(sShopData->tilemapBuffers[2], offset1, offset2, src);
             BuyMenuDrawMapMetatileLayer(sShopData->tilemapBuffers[3], offset1, offset2, src + 4);
         }
+    }
 }
 
 static void BuyMenuDrawMapMetatileLayer(u16 *dest, s16 offset1, s16 offset2, const u16 *src)
